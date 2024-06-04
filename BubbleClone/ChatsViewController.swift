5f685ed792975d61af6fa21950f42fd072e79884
taek0622 <min4644@gmail.com>
2024-06-04 21:04:03 +0900

    init project

---
+++ BubbleClone/ChatsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatsViewController.swift
+//  BubbleClone
+//
+//  Created by 김민택 on 6/4/24.
+//
+
+import UIKit
+
+class ChatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

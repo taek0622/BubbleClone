089d1d3c77b0fc77ad03520782f2d7b4f7b5ca64
taek0622 <min4644@gmail.com>
2024-06-05 19:03:17 +0900

    Feat: Add FriendsTableViewCell
    - Friends 뷰에서 사용할 테이블 셀 추가

---
+++ BubbleClone/FriendsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FriendsTableViewCell.swift
+//  BubbleClone
+//
+//  Created by 김민택 on 6/5/24.
+//
+
+import UIKit
+
+class FriendsTableViewCell: UITableViewCell {
+
+    static let identifier = "FriendsTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

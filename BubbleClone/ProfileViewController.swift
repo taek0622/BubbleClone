0651c9f03a5d390c379eaee45a80821893bc2150
taek0622 <min4644@gmail.com>
2024-06-05 19:04:31 +0900

    Feat: Add ProfileViewController
    - 프로필 뷰 추가

---
+++ BubbleClone/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  BubbleClone
+//
+//  Created by 김민택 on 6/5/24.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

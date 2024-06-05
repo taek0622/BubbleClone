84c879552ee0d8b43fe4b5267d54b8e1b4b680db
taek0622 <min4644@gmail.com>
2024-06-05 19:03:45 +0900

    Design: Add FriendsViewCell UI
    - FriendsViewCell UI 컴포넌트 추가

--- BubbleClone/FriendsTableViewCell.swift
+++ BubbleClone/FriendsTableViewCell.swift
@@ -11,6 +11,23 @@ class FriendsTableViewCell: UITableViewCell {
 
     static let identifier = "FriendsTableViewCell"
 
+    private lazy var thumbnailImageView: UIImageView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+
+    private lazy var profileName: UILabel = {
+        $0.text = ""
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+
+    private lazy var statusMessage: UILabel = {
+        $0.text = ""
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UILabel())
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
